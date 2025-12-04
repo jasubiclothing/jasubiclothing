ea870b1b34fa8a093a012be35cfcc29f94d7b043
jasubiclothing <guptabibekkumar10@gmail.com>
2025-12-04 19:52:01 +0000

    Delete NEW directory

--- NEW/script.js
+++
@@ -1,7 +0,0 @@
-function showSection(id) {
-document.querySelectorAll('section').forEach(sec => sec.style.display = 'none');
-document.getElementById(id).style.display = 'block';
-}
-
-
-document.addEventListener('DOMContentLoaded', () => showSection('home'));
